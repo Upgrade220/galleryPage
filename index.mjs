761ff7adce984d14f8b99f3f3a68d6e7246e3159
Upgrade220 <kowalkof@yandex.ru>
2021-01-06 21:01:50 +0500

    init

---
+++ index.mjs
@@ -0,0 +1,29 @@
+import express from "express";
+import * as path from "path";
+import hbs from "express-handlebars";
+import cookieParser from "cookie-parser";
+
+const rootDir = process.cwd();
+const port = 3000;
+const app = express();
+
+app.use('/static', express.static('static'));
+app.use(cookieParser());
+
+app.set("view engine", "hbs");
+
+app.engine(
+    "hbs",
+    hbs({
+        extname: "hbs",
+        defaultView: "default",
+        layoutsDir: path.join(rootDir, "/views/layouts/"),
+        partialsDir: path.join(rootDir, "/views/partials/"),
+    })
+);
+
+app.get("/", (_, res) => {
+    res.sendFile(path.join(rootDir, "/static/html/index.html"));
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}`));

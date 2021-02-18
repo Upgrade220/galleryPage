fabfadcab5e3d2a06470bd88bb412f38e9374e05
Upgrade220 <kowalkof@yandex.ru>
2021-02-19 01:46:11 +0500

    files parsing and generation of pages

--- index.mjs
+++ index.mjs
@@ -1,4 +1,5 @@
 import fs from "fs";
+import http2 from "http2";
 import express from "express";
 import * as path from "path";
 import hbs from "express-handlebars";
@@ -8,51 +9,87 @@ const rootDir = process.cwd();
 const port = process.env.PORT || 5000;
 const app = express();
 
+const galleryPath = './static/gallery_pics/';
+const galleryPFileName = 'p.txt';
+const galleryMImgFileName = 'mainImg.txt';
+
 let models = [
-    { name: "First Kitty", paragraph: "Some text ♥", 
-    frontAvifImg: "model1.avif", frontWebpImg: "model1.webp", frontJpegImg: "model1.jpeg", 
-    backAvifImg: "model1back.avif", backWebpImg: "model1back.webp", backJpegImg: "model1back.jpg" },
-    { name: "Second Kitty", paragraph: "Some text ♥", 
-    frontAvifImg: "model2.avif", frontWebpImg: "model2.webp", frontJpegImg: "model2.jpg", 
-    backAvifImg: "model2back.avif", backWebpImg: "model2back.webp", backJpegImg: "model2back.jpg" },
-    { name: "Third Kitty", paragraph: "Some text ♥", 
-    frontAvifImg: "model3.avif", frontWebpImg: "model3.webp", frontJpegImg: "model3.jpeg", 
-    backAvifImg: "model3back.avif", backWebpImg: "model3back.webp", backJpegImg: "model3back.jpg" }
-]
-
-let galleries = [
-    {link: "0", paragraph:"Some Kitty", avifImg: "cat1.avif", webpImg: "cat1.webp", jpegImg: "cat1.jpg"},
-    {link: "1", paragraph:"Some Kitty", avifImg: "cat2.avif", webpImg: "cat2.webp", jpegImg: "cat2.jpg"},
-    {link: "2", paragraph:"Some Kitty", avifImg: "cat3.avif", webpImg: "cat3.webp", jpegImg: "cat3.jpg"}
-]
-
-let galleryPages = [
-    { pictures:
-            [{avifImg: "cat1.avif", webpImg: "cat1.webp", jpegImg: "cat1.jpg", name: "cat1"},
-                {avifImg: "cat2.avif", webpImg: "cat2.webp", jpegImg: "cat2.jpg", name: "cat2"},
-                {avifImg: "cat3.avif", webpImg: "cat3.webp", jpegImg: "cat3.jpg", name: "cat3"}],
-        pageTitle: "0",
-        pageP: "Cute kitties"},
-    { pictures:
-            [{avifImg: "cat2.avif", webpImg: "cat2.webp", jpegImg: "cat2.jpg", name: "cat2"},
-                {avifImg: "cat3.avif", webpImg: "cat3.webp", jpegImg: "cat3.jpg", name: "cat3"},
-                {avifImg: "cat4.avif", webpImg: "cat4.webp", jpegImg: "cat4.jpg", name: "cat4"}],
-        pageTitle: "1",
-        pageP: "Cute kitties"},
-    { pictures:
-            [{avifImg: "cat3.avif", webpImg: "cat3.webp", jpegImg: "cat3.jpg", name: "cat3"},
-                {avifImg: "cat4.avif", webpImg: "cat4.webp", jpegImg: "cat4.jpg", name: "cat4"},
-                {avifImg: "cat5.avif", webpImg: "cat5.webp", jpegImg: "cat5.jpg", name: "cat5"}],
-        pageTitle: "2",
-        pageP: "Cute kitties"}
+    {
+        name: "First Kitty", paragraph: "Some text ♥",
+        frontAvifImg: "model1.avif", frontWebpImg: "model1.webp", frontJpegImg: "model1.jpeg",
+        backAvifImg: "model1back.avif", backWebpImg: "model1back.webp", backJpegImg: "model1back.jpg"
+    },
+    {
+        name: "Second Kitty", paragraph: "Some text ♥",
+        frontAvifImg: "model2.avif", frontWebpImg: "model2.webp", frontJpegImg: "model2.jpg",
+        backAvifImg: "model2back.avif", backWebpImg: "model2back.webp", backJpegImg: "model2back.jpg"
+    },
+    {
+        name: "Third Kitty", paragraph: "Some text ♥",
+        frontAvifImg: "model3.avif", frontWebpImg: "model3.webp", frontJpegImg: "model3.jpeg",
+        backAvifImg: "model3back.avif", backWebpImg: "model3back.webp", backJpegImg: "model3back.jpg"
+    }
 ]
 
 const getDirectories = source =>
     fs.readdirSync(source, { withFileTypes: true })
         .filter(dirent => dirent.isDirectory())
-        .map(dirent => dirent.name)
+        .map(dirent => dirent.name);
 
-console.log(getDirectories('./static'));
+const getFiles = source =>
+    fs.readdirSync(source, { withFileTypes: true })
+        .filter(dirent => dirent.isFile())
+        .map(dirent => dirent.name);
+
+const parseGalleryDirs = function (path) {
+    let result = [];
+    let dirs = getDirectories(path);
+    for (let dir of dirs) {
+        let dirContent = { pictures: [], pageTitle: '', pageP: '', mainImg: '' };
+        let dirFiles = getFiles(path + dir).sort();
+        let jpegImgs = [];
+        let webpImgs = [];
+        let avifImgs = [];
+        for (let file of dirFiles) {
+            let ext = file.split('.').pop();;
+            switch (ext) {
+                case 'jpg':
+                    jpegImgs.push(file);
+                    break;
+                case 'webp':
+                    webpImgs.push(file);
+                    break;
+                case 'avif':
+                    avifImgs.push(file);
+                    break;
+            }
+        }
+        if (jpegImgs.length === webpImgs.length && jpegImgs.length === avifImgs.length) {
+            for (let i = 0; i < jpegImgs.length; i++) {
+                dirContent.pictures.push({ avifImg: avifImgs[i], webpImg: webpImgs[i], jpegImg: jpegImgs[i], name: jpegImgs[i].replace('.jpg', '') })
+            }
+            dirContent.pageTitle = dir;
+            dirContent.pageP = fs.readFileSync(path + dir + '/' + galleryPFileName, 'utf-8');
+            dirContent.mainImg = fs.readFileSync(path + dir + '/' + galleryMImgFileName, 'utf-8');
+            result.push(dirContent);
+        }
+    }
+    return result;
+}
+
+const parseGalleries = function (galleryPages) {
+    let result = [];
+    let i = 0;
+    for (let page of galleryPages) {
+        let p = { link: i, paragraph: page.pageTitle, avifImg: `${page.mainImg}.avif`, webpImg: `${page.mainImg}.webp`, jpegImg: `${page.mainImg}.jpg` }
+        i++;
+        result.push(p);
+    }
+    return result;
+}
+
+let galleryPages = parseGalleryDirs(galleryPath);
+let galleries = parseGalleries(galleryPages);
 
 app.use("/static", express.static(path.join(rootDir, 'static')));
 app.use(cookieParser());
